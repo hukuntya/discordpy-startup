a1bab048b8d8651215466fa0841cdbb88516c772
hukuntya <74387548+hukuntya@users.noreply.github.com>
2021-06-04 18:32:53 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,28 +1,14 @@
-from discord.ext import commands
+import discord 
 import os
 import traceback
-import discord
 
-bot = commands.Bot(command_prefix='/')
+bot = discord.Client() 
 token = os.environ['DISCORD_BOT_TOKEN']
 
-
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
-
-
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pang')
-    
 @client.event
 async def on_message(message):
     if message.author != client.user:
         msg = message.content
-        await client.send_message(message.channel, msg)
-
+        await message.channel.send(msg)
 
 bot.run(token)
