6a50e733555855aa518e2537a966330c19bd1c16
hukuntya <74387548+hukuntya@users.noreply.github.com>
2021-06-04 19:05:02 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -11,8 +11,20 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
-
-    if message.content.count('すぐ消') > 0:
+    
+    out = ['すぐ消', 'すぐけす', 'すぐけし', '後で消', 'あとで消', 'あとでけす', 'あとでけし']
+    flag = False
+    
+    for s in out:
+        if if message.content.count(out) > 0:
+            flag = True
+    
+    if flag:
         await message.channel.send('言ったな！')
+    
+    if message.attachments:
+        for attachment in message.attachments:
+            if attachment.url.endswith(("png", "jpg", "jpeg")):
+                await message.channel.send(attachment.url)
 
 client.run(os.environ['DISCORD_BOT_TOKEN'])
