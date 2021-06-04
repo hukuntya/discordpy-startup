c18ec5d29e4a05cce2996dfd41532549496d6f38
hukuntya <74387548+hukuntya@users.noreply.github.com>
2021-06-04 19:18:28 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -3,6 +3,17 @@ import os
 
 client = discord.Client()
 
+def out(mess):
+    out = ['すぐ消', 'すぐけす', 'すぐけし', '後で消', 'あとで消', 'あとでけす', 'あとでけし']
+    flag = False
+    
+    for s in out:
+        if mess.content.count(s) > 0:
+            flag = True
+            
+    return flag
+    
+
 @client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
@@ -12,19 +23,10 @@ async def on_message(message):
     if message.author == client.user:
         return
     
-    out = ['すぐ消', 'すぐけす', 'すぐけし', '後で消', 'あとで消', 'あとでけす', 'あとでけし']
-    flag = False
-    
-    for s in out:
-        if message.content.count(s) > 0:
-            flag = True
-    
-    if flag:
-        await message.channel.send('言ったな！')
-    
-#     if message.attachments:
-#         for attachment in message.attachments:
-#             if attachment.url.endswith(("png", "jpg", "jpeg")):
-#                 await message.channel.send(attachment.url)
+    if message.attachments:
+        for attachment in message.attachments:
+            if attachment.url.endswith(("png", "jpg", "jpeg")):
+                if out(message):
+                    await message.channel.send(attachment.url)
 
 client.run(os.environ['DISCORD_BOT_TOKEN'])
