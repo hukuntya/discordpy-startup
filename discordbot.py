1d98ab215e12942d85fdc001053d7a3b7fcf33c6
hukuntya <74387548+hukuntya@users.noreply.github.com>
2021-06-04 18:54:45 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -12,7 +12,7 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.find('すぐ消') > 0 OR message.content.find('すぐけす') > 0 OR message.content.find('すぐけし') > 0:
+    if message.content in 'すぐ消':
         await message.channel.send('言ったな！')
 
 client.run(os.environ['DISCORD_BOT_TOKEN'])
