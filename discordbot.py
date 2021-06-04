cb6960f381aca3862b74617180cdd4fd79267eaf
hukuntya <74387548+hukuntya@users.noreply.github.com>
2021-06-04 18:42:19 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -2,13 +2,15 @@ import discord
 import os
 import traceback
 
-bot = discord.Client() 
+bot = discord.Client()
 token = os.environ['DISCORD_BOT_TOKEN']
 
 @client.event
 async def on_message(message):
-    if message.author != client.user:
-        msg = message.content
-        await message.channel.send(msg)
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
 
 bot.run(token)
